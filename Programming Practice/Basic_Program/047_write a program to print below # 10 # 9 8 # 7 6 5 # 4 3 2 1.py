3d56408bc0f66b9bb1526a0784ff3af7470ed9f4
Gaurav Kadhane <gakadhane@gmail.com>
2025-02-23 19:16:02 +0530

    python program practice

---
+++ Programming Practice/Basic_Program/047_write a program to print below # 10 # 9 8 # 7 6 5 # 4 3 2 1.py
@@ -0,0 +1,31 @@
+def print_pattern():
+    num = 10
+    while num > 0:
+        print("#", end=" ")
+        for _ in range(3):
+            if num > 0:
+                print(num, end=" ")
+                num -= 1
+        print()
+
+print_pattern()
+
+def print_pattern():
+    num = 10
+    count = 1
+    while num > 0:
+        print("#", end=" ")
+        for _ in range(count):
+            if num > 0:
+                print(num, end=" ")
+                num -= 1
+        print()
+        count += 1
+
+print_pattern()
+
+#Output
+# 10
+# 9 8
+# 7 6 5
+# 4 3 2 1
